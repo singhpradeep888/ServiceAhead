bfcb0eb991e1609ad9581cf397b75f49bf5906e7
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2024-09-05 21:05:01 +0530

    Project structured initiated

---
+++ src/main/java/com/alextechsolutions/ServiceAhead/ServiceAheadApplication.java
@@ -0,0 +1,13 @@
+package com.alextechsolutions.ServiceAhead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAheadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alextechsolutions/ServiceAhead/ServiceAheadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alextechsolutions.ServiceAhead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAheadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
